c3acb236979dcc05276648e0a469fae35ddb091c
Road-hog123 <contact.roadhog123@gmail.com>
2018-11-08 23:51:18 +0000

    Remove settings wheel (hardcode settings instead)

--- chat-monitor.user.js
+++ chat-monitor.user.js
@@ -3,19 +3,17 @@
 // @namespace      https://roadhog123.co.uk
 // @description    adds inline images to Twitch chat
 // @match        https://www.twitch.tv/popout/*/chat?display*
-// @version    0.302-RH1
+// @version    0.302-RH2
 // @updateURL https://raw.githubusercontent.com/road-hog123/significantly-less-nifty-chat/master/chat-monitor.user.js
 // @downloadURL https://raw.githubusercontent.com/road-hog123/significantly-less-nifty-chat/master/chat-monitor.user.js
 // @require  https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js
 // @require  https://gist.github.com/raw/2625891/waitForKeyElements.js
 // @grant       GM_getResourceText
 // @grant       GM_addStyle
-// @require  https://raw.githubusercontent.com/sizzlemctwizzle/GM_config/master/gm_config.js
 // @grant       GM_getValue
 // @grant       GM_setValue
 // @grant       GM_log
 // @resource style https://raw.githubusercontent.com/road-hog123/significantly-less-nifty-chat/master/chat-monitor.css
-// @resource material-icons https://fonts.googleapis.com/icon?family=Material+Icons
 // ==/UserScript==
 
 // Redirect to new layout if we find the old layout
@@ -35,97 +33,9 @@ var qs = getQS(location.search);
 
 var inlineImages = false;
 
-//Create config page fields
-//Fields appear in the order written below.
-//Maybe in the future this could be loaded from a JSON file? It would be easier to maintain.
-var configFields = {
-    "HideChatInput": { //Id for field in html
-        "label" : "Hide Chat Input Area", //Label that appears on the config
-        "type" : "checkbox",
-        "default" : true
-    },
-    "ReverseDirection": {
-        "label" : "New messages appear on top",
-        "type" : "checkbox",
-        "default" : true
-    },
-    "InlineImages": {
-        "label" : "Display images that are linked",
-        "type" : "checkbox",
-        "default" : true
-    },
-    "SmoothScroll": {
-        "label": "Make new messages slide in smoothly (only works when messages appear on top)",
-        "type": "checkbox",
-        "default": true
-    },
-    "SmoothScrollSpeed": {
-        "label": "Time needed for a new message to slide in (seconds)",
-        "type": "float",
-        "default": "1"
-    },
-    "UsernamesToHighlight": {
-        "label" : "Username(s)",
-        "section" : ["Username Highlighting", "Change the color of any of the usernames listed below. Separate with commas"],
-        "type" : "text",
-        "title" : "seperate usernames with commas, case sensitive",
-        //These as defaults as I can't imagine Paul will mind, and I know I don't mind.
-        "default" : "ThingsOnMyStream, cheetoJack"
-    },
-    "UsernameHighlightingColor": {
-        "label" : "Highlight Color",
-        "type" : "text",
-        "default" : "#ff6696" //Its pink. Its a good color bront.
-    },
-    "KeywordsToHighlight": {
-        "label" : "Keyword(s)",
-        "section" : ["Keyword Highlighting", "Change the background color of any messages that use the keywords listed below. Separate with commas"],
-        "type" : "text",
-        "title" : "seperate keywords with commas, case sensitive",
-        "default" : ""
-    },
-    "KeywordHighlightingBackgroundColor": {
-        "label" : "Highlight Background Color",
-        "type" : "text",
-        "default" : "#560b33" //Its also pink.
-    },
-    "CustomHighlighting": {
-        "label" : "",
-        "section" : ["CSS User Highlighting"],
-        "type" : "textarea",
-        //Keeping CSS in from chat-monitor-highlight.css as an example of what you can do
-        "default" : ".chat-line__message[data-badges*='Moderator'] .chat-author__display-name {\n" +
-                        "\tcolor: #8383f9 !important;\n" +
-                    "}\n" +
-                    ".chat-line__message[data-badges*='Broadcaster'] {\n" +
-                        "\tbackground-color: #000090 !important;\n" +
-                    "}\n" +
-                    ".chat-line__message[data-badges*='Broadcaster'] .chat-author__display-name {\n" +
-                        "\tcolor: #00b5e0 !important;\n" +
-                    "}\n" +
-                    ".chat-line__message[data-user='LRRbot'] .chat-author__display-name {\n" +
-                        "\tcolor:purple !important;\n" +
-                    "}\n" +
-                    ".chat-line__message[data-user='LRRbot'][data-message*='thanks for']{\n" +
-                        "\tbackground-color:purple !important;\n" +
-                    "}\n" +
-                    ".chat-line__message[data-user='LRRbot'][data-message*='thanks for'] .chat-author__display-name{\n" +
-                        "\tcolor:black !important;\n" +
-                    "}\n" +
-                    ".chat-line__message[data-message*='loadingreadyrun'] {\n" +
-                        "\tbackground-color: #00005d !important;\n" +
-                    "}"
-    },
-    "RefreshReminder": {
-        "label": "",
-        "section" : ["After saving, be sure to refresh the page to have your new settings apply!"],
-        "type" : "hidden"
-    }
-};
 var scrollDistance = 0,
     scrollReference = +new Date();
 
-initConfig();
 var MESSAGE_CONTAINER = '.chat-list .tw-full-height';
 waitForKeyElements(MESSAGE_CONTAINER, onChatLoad);
 var twitterScript = document.createElement("script");
@@ -138,105 +48,9 @@ function onChatLoad() {
     actionFunction();
 }
 
-//Creates , inits the config handler
-//See https://github.com/sizzlemctwizzle/GM_config/wiki for details
-function initConfig() {
-    GM_config.init({
-        "id" : "chat-config",
-        "title" : "Nifty Chat Monitor Settings",
-        "fields" : configFields,
-        // I need a better way of settings this css attribute. I'm thinking of converting
-        // it to a variable, because I could use new lines in that and keep it somewhere
-        // out of sight, but thats for later-Jack
-        "css" : "#chat-config .field_label { font-size: 32px !important; line-height: 35px !important; font-family: 'Open Sans Condensed', sans-serif !important; font-weight: normal !important; } .config_var input[type=checkbox] {  transform: scale(2.0); } #chat-config_field_CustomHighlighting { width: 90%; height: 500px; }" //adds CSS to the config page
-    });
-}
-
 //Checks all config options and loads them appropriately
 function loadSettings() {
-    //Add settings wheel to page
-    $('.chat-list').append("<div id=\"settings-wheel\"> <i class=\"material-icons\">settings</i> </div>");
-    $('#settings-wheel').click(function() {
-        GM_config.open();
-    });
-
-    //Reverse messages
-    if(typeof qs.reverse !== 'undefined' || GM_config.get("ReverseDirection")) {
-        document.querySelector(MESSAGE_CONTAINER).classList.add('reverse');
-    }
-
-    //Hide chat interface
-    if(GM_config.get("HideChatInput")) {
-        document.querySelector('.chat-input').classList.add('tw-hide');
-    }
-
-    //Check if we should be adding inline images or not
-    if(typeof qs.img !== 'undefined' || GM_config.get("InlineImages")) {
-        inlineImages = true;
-    }
-
-    //Handles UsernamesToHighlight, UsernameHighlightingColor, CustomHighlighting config fields
-    addCssFromConfig(generateCss());
-}
-
-//Adds the CSS from the config fields
-//generatedCss will be added to the CSS on the page.
-function addCssFromConfig(generatedCss) {
-    //Add CSS from text area
-    var customHighlighting = GM_config.get("CustomHighlighting");
-    var head = document.getElementsByTagName("head")[0];
-    var newCss = document.createElement("style");
-    newCss.type = "text/css";
-    newCss.innerHTML = generatedCss + "\n" + customHighlighting;
-    head.appendChild(newCss);
-}
-
-//Creates CSS from config fields
-function generateCss() {
-    //Stores CSS generated by various options
-    var generatedCss = "";
-    generatedCss += generateUsernameHighlightingCss();
-    generatedCss += generateKeywordHighlightingCss();
-    return generatedCss;
-}
-
-//Handles UsernamesToHighlight section, returns CSS as a string
-function generateUsernameHighlightingCss() {
-    var generatedCss = "";
-    //Load config options, Check if contains data
-    var usernamesToHighlight = GM_config.get("UsernamesToHighlight");
-    var usernameHighlightColor = GM_config.get("UsernameHighlightingColor");
-    if(usernamesToHighlight && usernameHighlightColor) {
-        //Split into array on commas
-        var usernameList = usernamesToHighlight.split(",");
-        //Adds rule for each username. This could just add one rule, and save a bit of space, but I think this works just as well.
-        for(var i = 0; i < usernameList.length; i++) {
-            //Add css to variable
-            generatedCss += ".chat-line__message[data-user=\"" + usernameList[i].trim() + "\"] .chat-author__display-name {\n\tcolor: " + usernameHighlightColor + " !important;\n }\n";
-        }
-    }
-    return generatedCss;
-}
-
-//Handles KeywordsToHighlight section, returns CSS as a string
-function generateKeywordHighlightingCss() {
-    var generatedCss = "";
-    //Load config options, Requires either Text or Background color, but not both
-    //data-message attribute makes everything lowercase, so we do the same to our keywords.
-    var keywordsToHighlight = GM_config.get("KeywordsToHighlight").toLowerCase();
-    var keywordHighlightBackgroundColor = GM_config.get("KeywordHighlightingBackgroundColor");
-    if(keywordsToHighlight && keywordHighlightBackgroundColor) {
-        //Split into array on commas
-        var keywordList = keywordsToHighlight.split(",");
-        //Adds rule for each username. This could just add one rule, and save a bit of space, but I think this works just as well.
-        for(var i = 0; i < keywordList.length; i++) {
-            //Add css to variable
-            generatedCss += ".chat-line__message[data-message*=\"" + keywordList[i] + "\"] {\n";
-            generatedCss += "\tbackground-color: " + keywordHighlightBackgroundColor + " !important;\n";
-            generatedCss += "}\n";
-        }
-    }
-    return generatedCss;
+    inlineImages = true;
 }
 
 function actionFunction() {
@@ -363,5 +177,3 @@ function actionFunction() {
 //inject custom stylessheet
 var style = GM_getResourceText('style');
 GM_addStyle(style);
-var materialIcons = GM_getResourceText('material-icons');
-GM_addStyle(materialIcons);
